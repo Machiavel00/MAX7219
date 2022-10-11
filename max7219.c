c031cf3d257634318cae5c9d1f82aa7d3458fc29
mledimna <mathieu-ledimna@outlook.com>
2022-10-11 19:41:19 +0200

    Reformat code

--- max7219.c
+++ max7219.c
@@ -7,39 +7,42 @@
 
 #include "max7219.h"
 
-#define RESET_PARAMS()	do{								\
-							hspi = NULL;				\
-							spi_ncs_port = NULL;		\
-							spi_ncs_pin = GPIO_PIN_0;	\
-							digits_count = 0;			\
-						} while(0)						\
+#define RESET_PARAMS()            \
+	do                            \
+	{                             \
+		hspi = NULL;              \
+		spi_ncs_port = NULL;      \
+		spi_ncs_pin = GPIO_PIN_0; \
+		digits_count = 0;         \
+	} while (0)
 
-#define CHECK_PARAMS()	do{																\
-							if ((hspi == NULL) 			||								\
-								(spi_ncs_port == NULL) 	||								\
-								(digits_count>MAX_DIGITS_COUNT || digits_count<0)){		\
-								RESET_PARAMS();											\
-								return HAL_ERROR;										\
-							}															\
-						} while(0)														\
-
-
+#define CHECK_PARAMS()                                             \
+	do                                                             \
+	{                                                              \
+		if ((hspi == NULL) ||                                      \
+			(spi_ncs_port == NULL) ||                              \
+			(digits_count > MAX_DIGITS_COUNT || digits_count < 0)) \
+		{                                                          \
+			RESET_PARAMS();                                        \
+			return HAL_ERROR;                                      \
+		}                                                          \
+	} while (0)
 
 /* Static variables used to store MAX7219 related objects */
-static SPI_HandleTypeDef * hspi;		// SPI handle to send commands over SPI port
-static GPIO_TypeDef  * spi_ncs_port;	// GPIO port of NCS signal
-static uint16_t spi_ncs_pin;			// GPIO pin of NCS signal
-static uint8_t digits_count;			// Number of digits to drive using MAX7219
+static SPI_HandleTypeDef *hspi;	   // SPI handle to send commands over SPI port
+static GPIO_TypeDef *spi_ncs_port; // GPIO port of NCS signal
+static uint16_t spi_ncs_pin;	   // GPIO pin of NCS signal
+static uint8_t digits_count;	   // Number of digits to drive using MAX7219
 
 static uint8_t digits_registers[] = {
-		DIGIT_0_REG_BASE,
-		DIGIT_1_REG_BASE,
-		DIGIT_2_REG_BASE,
-		DIGIT_3_REG_BASE,
-		DIGIT_4_REG_BASE,
-		DIGIT_5_REG_BASE,
-		DIGIT_6_REG_BASE,
-		DIGIT_7_REG_BASE,
+	DIGIT_0_REG_BASE,
+	DIGIT_1_REG_BASE,
+	DIGIT_2_REG_BASE,
+	DIGIT_3_REG_BASE,
+	DIGIT_4_REG_BASE,
+	DIGIT_5_REG_BASE,
+	DIGIT_6_REG_BASE,
+	DIGIT_7_REG_BASE,
 };
 
 /*
@@ -47,10 +50,11 @@ static uint8_t digits_registers[] = {
  * @param _address Address on 8 bits
  * @param _data Data on 8 bits
  */
-static HAL_StatusTypeDef max7219_transmit(uint8_t _address, uint8_t _data){
-	uint8_t data[] = {_address, _data};					// SPI transmit buffer
-	size_t data_sz = sizeof(data) / sizeof(uint8_t);	// Size of SPI transmit buffer
-	HAL_StatusTypeDef max7219_status = HAL_OK;			// Return value
+static HAL_StatusTypeDef max7219_transmit(uint8_t _address, uint8_t _data)
+{
+	uint8_t data[] = {_address, _data};				 // SPI transmit buffer
+	size_t data_sz = sizeof(data) / sizeof(uint8_t); // Size of SPI transmit buffer
+	HAL_StatusTypeDef max7219_status = HAL_OK;		 // Return value
 
 	// Select MAX7219, send data, de-select MAX7219
 	HAL_GPIO_WritePin(spi_ncs_port, spi_ncs_pin, GPIO_PIN_RESET);
@@ -60,7 +64,6 @@ static HAL_StatusTypeDef max7219_transmit(uint8_t _address, uint8_t _data){
 	// Return transmit status
 	return max7219_status;
 }
-
 
 /**
  * @brief Init function. Pass hardware handles and constants.
@@ -72,7 +75,8 @@ static HAL_StatusTypeDef max7219_transmit(uint8_t _address, uint8_t _data){
  * @retval HAL status, HAL_OK if pass occured, HAL_ERROR otherwise
  *
  */
-HAL_StatusTypeDef max7219_init(SPI_HandleTypeDef * _hspi, GPIO_TypeDef  * _spi_ncs_port, uint16_t _spi_ncs_pin, uint8_t _digits_count){
+HAL_StatusTypeDef max7219_init(SPI_HandleTypeDef *_hspi, GPIO_TypeDef *_spi_ncs_port, uint16_t _spi_ncs_pin, uint8_t _digits_count)
+{
 	/* Attribute parameters to static variables */
 	hspi = _hspi;
 	spi_ncs_port = _spi_ncs_port;
@@ -86,37 +90,42 @@ HAL_StatusTypeDef max7219_init(SPI_HandleTypeDef * _hspi, GPIO_TypeDef  * _spi_n
 
 	// Shutdown MAX7219 to reset configuration
 	max7219_status = max7219_transmit(SHUTDOWN_REG_BASE, SHUTDOWN_REG_SHUTDOWN_MODE);
-	if (max7219_status != HAL_OK) return max7219_status;
+	if (max7219_status != HAL_OK)
+		return max7219_status;
 
 	HAL_Delay(1000);
 
 	// Enable MAX7219
 	max7219_status = max7219_transmit(SHUTDOWN_REG_BASE, SHUTDOWN_REG_NORMAL_MODE);
-	if (max7219_status != HAL_OK) return max7219_status;
+	if (max7219_status != HAL_OK)
+		return max7219_status;
 
 	// Set scan limit to number of digits
-	max7219_status = max7219_transmit(SCAN_LIMIT_REGG_BASE, digits_count-1);
-	if (max7219_status != HAL_OK) return max7219_status;
+	max7219_status = max7219_transmit(SCAN_LIMIT_REGG_BASE, digits_count - 1);
+	if (max7219_status != HAL_OK)
+		return max7219_status;
 
 	// Set decode mode to 'no decode'
 	max7219_status = max7219_transmit(DECODE_MODE_REG_BASE, 0x00);
-	if (max7219_status != HAL_OK) return max7219_status;
+	if (max7219_status != HAL_OK)
+		return max7219_status;
 
 	// Set brightness to middle value
 	max7219_status = max7219_transmit(INTENSITY_REG_BASE, 0x08);
-	if (max7219_status != HAL_OK) return max7219_status;
+	if (max7219_status != HAL_OK)
+		return max7219_status;
 
 	// Erase all digits
-	for (int i=0; i<digits_count; i++){
+	for (int i = 0; i < digits_count; i++)
+	{
 		max7219_status = max7219_transmit(digits_registers[i], DIGIT_OFF);
-		if (max7219_status != HAL_OK) return max7219_status;
+		if (max7219_status != HAL_OK)
+			return max7219_status;
 	}
-
 
 	// Return on success
 	return HAL_OK;
 }
-
 
 /**
  * @brief Display value without code B decoding.
@@ -124,18 +133,21 @@ HAL_StatusTypeDef max7219_init(SPI_HandleTypeDef * _hspi, GPIO_TypeDef  * _spi_n
  * @param _digit_value Desired digit value to be written
  * @retval HAL_OK on success
  */
-HAL_StatusTypeDef max7219_display_no_decode(uint8_t _digit_index, uint8_t _digit_value){
+HAL_StatusTypeDef max7219_display_no_decode(uint8_t _digit_index, uint8_t _digit_value)
+{
 	HAL_StatusTypeDef max7219_status = HAL_OK;
 
 	// Check if init has been called
 	CHECK_PARAMS();
 
 	/* Check if digit index does not overflow actual hardware setup */
-	if (_digit_index > digits_count) return HAL_ERROR;
+	if (_digit_index > digits_count)
+		return HAL_ERROR;
 
 	// Display value
 	max7219_status = max7219_transmit(digits_registers[_digit_index], _digit_value);
-	if (max7219_status != HAL_OK) return max7219_status;
+	if (max7219_status != HAL_OK)
+		return max7219_status;
 
 	return max7219_status;
 }
--- max7219.h
+++ max7219.h
@@ -8,48 +8,40 @@
 #ifndef MAX7219_MAX7219_H_
 #define MAX7219_MAX7219_H_
 
-
-
 #include "stm32l1xx_hal.h"
 
-
-
-#define MAX_DIGITS_COUNT	8
+#define MAX_DIGITS_COUNT 8
 
 /**
  * @brief MAX7219 registers
  */
-#define DIGIT_0_REG_BASE		((uint8_t)0x01)
-#define DIGIT_1_REG_BASE		((uint8_t)0x02)
-#define DIGIT_2_REG_BASE		((uint8_t)0x03)
-#define DIGIT_3_REG_BASE		((uint8_t)0x04)
-#define DIGIT_4_REG_BASE		((uint8_t)0x05)
-#define DIGIT_5_REG_BASE		((uint8_t)0x06)
-#define DIGIT_6_REG_BASE		((uint8_t)0x07)
-#define DIGIT_7_REG_BASE		((uint8_t)0x08)
-#define DECODE_MODE_REG_BASE	((uint8_t)0x09)
-#define INTENSITY_REG_BASE		((uint8_t)0x0A)
-#define SCAN_LIMIT_REGG_BASE	((uint8_t)0x0B)
-#define SHUTDOWN_REG_BASE		((uint8_t)0x0C)
-#define DISPLAY_TEST_REG_BASE	((uint8_t)0x0F)
+#define DIGIT_0_REG_BASE ((uint8_t)0x01)
+#define DIGIT_1_REG_BASE ((uint8_t)0x02)
+#define DIGIT_2_REG_BASE ((uint8_t)0x03)
+#define DIGIT_3_REG_BASE ((uint8_t)0x04)
+#define DIGIT_4_REG_BASE ((uint8_t)0x05)
+#define DIGIT_5_REG_BASE ((uint8_t)0x06)
+#define DIGIT_6_REG_BASE ((uint8_t)0x07)
+#define DIGIT_7_REG_BASE ((uint8_t)0x08)
+#define DECODE_MODE_REG_BASE ((uint8_t)0x09)
+#define INTENSITY_REG_BASE ((uint8_t)0x0A)
+#define SCAN_LIMIT_REGG_BASE ((uint8_t)0x0B)
+#define SHUTDOWN_REG_BASE ((uint8_t)0x0C)
+#define DISPLAY_TEST_REG_BASE ((uint8_t)0x0F)
 
 /**
  * @brief MAX7219 registers definitions
  */
-#define SHUTDOWN_REG_SHUTDOWN_MODE		((uint8_t)0x00)
-#define SHUTDOWN_REG_NORMAL_MODE		((uint8_t)0x01)
+#define SHUTDOWN_REG_SHUTDOWN_MODE ((uint8_t)0x00)
+#define SHUTDOWN_REG_NORMAL_MODE ((uint8_t)0x01)
 
 /**
  * @brief Digits values
  */
-#define DIGIT_OFF	((uint8_t)0b00000000)
-#define DIGIT_ON	((uint8_t)0b11111111)
+#define DIGIT_OFF ((uint8_t)0b00000000)
+#define DIGIT_ON ((uint8_t)0b11111111)
 
-
-
-HAL_StatusTypeDef max7219_init(SPI_HandleTypeDef * _hspi, GPIO_TypeDef  * _spi_ncs_port, uint16_t _spi_ncs_pin, uint8_t _digits_count);
+HAL_StatusTypeDef max7219_init(SPI_HandleTypeDef *_hspi, GPIO_TypeDef *_spi_ncs_port, uint16_t _spi_ncs_pin, uint8_t _digits_count);
 HAL_StatusTypeDef max7219_display_no_decode(uint8_t _digit_index, uint8_t _digit_value);
-
-
 
 #endif /* MAX7219_MAX7219_H_ */
